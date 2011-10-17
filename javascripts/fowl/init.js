0a342c12794655993ba1d023d4987a1ceebca24f
Anton Byrna <it.release@gmail.com>
2011-10-17 16:11:08 +0400

    changes

--- javascripts/fowl/init.js
+++ javascripts/fowl/init.js
@@ -10,18 +10,18 @@ fowl.getMsg = function( str, opt_values ){
 };
 
 fowl.parse = function( tweet ){
-  // parse urls
+  // urls
   tweet = tweet.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function(url) {
   	return url.link(url);
   });
   
-  // parse usernames
+  // usernames
   tweet = tweet.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
-  	var username = u.replace("@","");
+  	var username = u.replace('@', '');
   	return u.link('http://twitter.com/' + username);
   });
   
-  // parse hashtags
+  // hashtags
   tweet = tweet.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
   	var tag = t.replace('#', '%23');
   	return t.link('http://search.twitter.com/search?q=' + tag);
@@ -30,43 +30,41 @@ fowl.parse = function( tweet ){
   return tweet;
 };
 
-fowl.init = function(){
-  fowl.pubsub.subscribe(fowl.timeline.EventType.HOME, function( status, tweets ){
-    var tweetTpl = '<span class="nickname">{$username}</span>';
-        tweetTpl += '<time datetime="{$datetime}">{$time}</time>';
-        tweetTpl += '<p>{$text}</p>';
-    
-    var documentFragment = document.createDocumentFragment();
-    
-    tweets.forEach( function( tweet ){
-      var date = new Date( tweet.created_at );
-      
-      var data = {
-        username: tweet.user.screen_name,
-        datetime: date.toFormattedString('yyyy-MM-dd HH:mm:ss'),
-        time: humanized_time_span( date ),
-        text: fowl.parse( tweet.text )
-      };
-      
-      var li = document.createElement( 'li' );
-      li.id = tweet.id_str;
-      li.innerHTML = fowl.getMsg( tweetTpl, data );
-      
-      documentFragment.appendChild( li );
-    } );
-    
-    var homeTimeline = document.getElementById('tweetsList'),
-        slide = homeTimeline.parentNode;
-    
-    
-  });
-  
-  var timeline = fowl.storage.get('timeline') || {};
-  
-  fowl.timeline.home.init( timeline['home'] );
-  fowl.timeline.home.fetch();
-  
-  setInterval(function(){
-    fowl.timeline.home.fetch();
-  }, 60 * 1000);
-};
+// fowl.init = function(){
+//   fowl.pubsub.subscribe(fowl.timeline.EventType.HOME, function( status, tweets ){
+//     var tweetTpl = '<span class="nickname">{$username}</span>';
+//         tweetTpl += '<time datetime="{$datetime}">{$time}</time>';
+//         tweetTpl += '<p>{$text}</p>';
+//     
+//     var documentFragment = document.createDocumentFragment();
+//     
+//     tweets.forEach( function( tweet ){
+//       var date = new Date( tweet.created_at );
+//       
+//       var data = {
+//         username: tweet.user.screen_name,
+//         datetime: date.toFormattedString('yyyy-MM-dd HH:mm:ss'),
+//         time: humanized_time_span( date ),
+//         text: fowl.parse( tweet.text )
+//       };
+//       
+//       var li = document.createElement( 'li' );
+//       li.id = tweet.id_str;
+//       li.innerHTML = fowl.getMsg( tweetTpl, data );
+//       
+//       documentFragment.appendChild( li );
+//     } );
+//     
+//     var homeTimeline = document.getElementById('tweetsList'),
+//         slide = homeTimeline.parentNode;
+//   });
+//   
+//   var timeline = fowl.storage.get('timeline') || {};
+//   
+//   fowl.timeline.home.init( timeline['home'] );
+//   fowl.timeline.home.fetch();
+//   
+//   setInterval(function(){
+//     fowl.timeline.home.fetch();
+//   }, 60 * 1000);
+// };
--- javascripts/fowl/storage.js
+++ javascripts/fowl/storage.js
@@ -1,21 +1,32 @@
 fowl = window.fowl || {};
 
 fowl.storage = (function(){
-  var storage = JSON.parse( localStorage.getItem('fowl') ) || {};
-  
   function get( key ){
+    var storage = JSON.parse( localStorage.getItem('fowl') ) || {};
     return storage[key];
   }
   
   function set( key, value ){
-    storage[key] = value;
-    localStorage.removeItem('fowl');
+    var storage = JSON.parse( localStorage.getItem('fowl') ) || {};
+    if( typeof key == 'object' ){
+      for( var prop in key ){
+        if( key.hasOwnProperty( prop ) ){
+          storage[ prop ] = key[ prop ];
+        }
+      }
+    }
+    else{
+      storage[ key ] = value;
+    }
+    
     localStorage.setItem('fowl', JSON.stringify( storage ) );
   }
   
   function remove( key ){
+    var storage = JSON.parse( localStorage.getItem('fowl') ) || {};
     storage[key] = null;
     delete storage[key];
+    localStorage.setItem('fowl', JSON.stringify( storage ) );
   }
   
   return {
